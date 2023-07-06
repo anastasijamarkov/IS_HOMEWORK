880af7f738bd1a80459f7917ca9ab9995f6e9442
anastasijamarkov <132066486+anastasijamarkov@users.noreply.github.com>
2023-07-06 11:07:35 +0200

    Added is_homework files to repository

---
+++ MovieTicketShopAdminApplication/MovieTicketShopAdminApplication/Models/Ticket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketShopAdminApplication.Models
+{
+    public class Ticket
+    {
+        public Guid Id { get; set; }
+        public string MovieName { get; set; }
+        public string MovieImage { get; set; }
+        
+        public string MovieDescription { get; set; }
+        public string Genre { get; set; }
+        public int TicketPrice { get; set; }
+
+        public DateTime DateValid { get; set; }
+
+        public int Rating { get; set; }
+    }
+}
---
+++ MovieTicketShopAdminApplication/MovieTicketShopAdminApplication/Models/TicketShopApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketShopAdminApplication.Models
+{
+    public class TicketShopApplicationUser
+    {
+        public string NormalizedEmail { get; set; }
+        public string Email { get; set; }
+        public string NormalizedUserName { get; set; }
+        public string Role { get; set; }
+        public string UserName { get; set; }
+    }
+}
